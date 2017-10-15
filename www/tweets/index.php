f381b30ed4b94f0453d2f06dd8f2f3e2e505cc8f
Mikhail Miropolskiy <the-ms@ya.ru>
2017-10-15 10:47:01 +0700

    добавление

--- www/tweets/index.php
+++ www/tweets/index.php
@@ -1,13 +1,33 @@
 <?
+    error_reporting(E_ALL);
+    ini_set('display_startup_errors', 1);
+    ini_set('display_errors', 1);
+    ini_set('magic_quotes_gpc', 0);
+
+    header("Access-Control-Allow-Origin: *");
+
     require '../../vendor/autoload.php';
     require '../../app/Tweets.php';
 
     Tweets::connect();
-    $tweets = Tweets::sql('SELECT * FROM :table ORDER BY id DESC');
     $result = [];
-    foreach ($tweets as $tweet) {
+
+    if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+        $tweets = Tweets::sql('SELECT * FROM :table ORDER BY id DESC');
+
+        foreach ($tweets as $tweet) {
+            $result[] = $tweet->data;
+        }
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $data = json_decode(file_get_contents('php://input'));
+
+        $tweet = new Tweets();
+        $tweet->text = $data->text;
+        $tweet->date = date('Y-m-d');
+        $tweet->save();
         $result[] = $tweet->data;
     }
 
-    header("Access-Control-Allow-Origin: *");
     echo json_encode($result);
